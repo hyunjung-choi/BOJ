457e54d479c1a31a7a8487db26984598db96e1f9
hyunjung-choi <hjchoi.dev@gmail.com>
2021-11-14 21:34:33 +0900

    Add Q8958 OX퀴즈

---
+++ src/Q8958.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * OX퀴즈
+ * acmicpc.net/problem/8958
+ */
+
+public class Q8958 {
+    private static final StringBuilder sb = new StringBuilder();
+
+    private static void solution(char[] chars) {
+        int score = 0, sequence = 0;
+
+        for (char ch : chars) {
+            if (ch == 'O') score += ++sequence;
+            else sequence = 0;
+        }
+
+        sb.append(score).append("\n");
+    }
+
+    private static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) solution(br.readLine().toCharArray());
+
+        br.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        System.out.println(sb);
+    }
+}
