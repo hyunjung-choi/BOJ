dafc4f7500c9a95d24b627fe515c095e25a2b418
최현정 <hjchoi.dev@gmail.com>
2022-08-26 18:16:29 +0900

    :construction: DoIt/Q10986 나머지 합 (런타임 에러)

---
+++ src/DoIt/Q10986.java
@@ -0,0 +1,54 @@
+package DoIt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 나머지 합
+ * https://www.acmicpc.net/problem/10986
+ */
+
+public class Q10986 {
+
+    private static int N, M, answer;
+    private static long[] sum, count;
+
+    private static void solution() {
+        for (int i = 0; i < M; i++) {
+            if (count[i] != 0) answer += (count[i] * (count[i] - 1)) / 2;
+        }
+    }
+
+    private static void dividedByM() {
+        for (int i = 1; i <= N ; i++) {
+            int remainder = Long.valueOf(sum[i]).intValue() % M;
+            if (remainder == 0) answer++;
+            count[remainder]++;
+        }
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        sum = new long[N + 1];
+        count = new long[M];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            sum[i] += sum[i - 1] + Long.parseLong(st.nextToken());
+        }
+        dividedByM();
+        br.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+        System.out.println(answer);
+    }
+}
