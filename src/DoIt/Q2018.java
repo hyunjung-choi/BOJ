79df7bc6479e34553934ff9e5dbaa7cbddc6eea1
최현정 <hjchoi.dev@gmail.com>
2022-08-28 23:30:37 +0900

    ✨Add DoIt/Q2018 수들의 합 5

---
+++ src/DoIt/Q2018.java
@@ -0,0 +1,44 @@
+package DoIt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 수들의 합 5
+ * https://www.acmicpc.net/problem/2018
+ */
+
+public class Q2018 {
+
+    private static int N, sum = 1, start = 1, end = 1, answer = 1;
+
+    private static void solution() {
+        while (end < N) {
+            if (sum < N) {
+                end++;
+                sum += end;
+            }
+            else if (sum > N) {
+                sum -= start;
+                start++;
+            } else {
+                answer++;
+                end++;
+                sum += end;
+            }
+        }
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        br.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+        System.out.println(answer);
+    }
+}
