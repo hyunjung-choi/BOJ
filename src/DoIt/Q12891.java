50610bf497fb5165cb05c3b28182ce7d74c98790
최현정 <hjchoi.dev@gmail.com>
2022-09-10 23:38:09 +0900

    🚧Add DoIt/Q12891 DNA 비밀번호 슬라이딩 윈도우 까지는 구현 완료

--- src/DoIt/Q12891.java
+++ src/DoIt/Q12891.java
@@ -3,6 +3,8 @@ package DoIt;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 /**
@@ -11,74 +13,37 @@ import java.util.StringTokenizer;
  */
 
 public class Q12891 {
-    private static int S, P, checkSecretNumber, answer;
+    private static int S, P, checkSecretNumber, left, right, answer;
     private static char[] dna;
     private static int[] checkArr;
     private static int[] myCheckArr;
+    private static Map<Character, Integer> dnaMap = new HashMap<>();
 
     private static void solution() {
-        for (int i = 0; i < P; i++) {
-            Add(dna[i]);
-        }
-        if (checkSecretNumber == 4) answer++;
 
-        for (int i = P; i < S; i++) {
-            Add(dna[i]);
-            Remove(dna[i - P]);
+        for (int i = 0; i <= S - P; i++) {
+            Map<Character, Integer> myMap = new HashMap<>();
+            left = i;
+            for (int j = 0; j < P; j++) {
+
+                char ch = dna[left];
+                System.out.print(ch);
+
+                if (ch == 'A' || ch == 'C' || ch == 'G' || ch == 'T') {
+                    myMap.put(ch, myMap.getOrDefault(ch, 0) + 1);
+                }
+                left++;
+
+            }
+            right++;
+            System.out.println(myMap);
         }
 
-        if (checkSecretNumber == 4) answer++;
+        System.out.println(dnaMap);
+
         System.out.println(answer);
     }
 
-    private static void Remove(char c) {
-        switch (c) {
-            case 'A':
-                if (myCheckArr[0] == checkArr[0]) checkSecretNumber--;
-                myCheckArr[0]--;
-                break;
-
-            case 'C':
-                if (myCheckArr[1] == checkArr[1]) checkSecretNumber--;
-                myCheckArr[1]--;
-                break;
-
-            case 'G':
-                if (myCheckArr[2] == checkArr[2]) checkSecretNumber--;
-                myCheckArr[2]--;
-                break;
-
-            case 'T':
-                if (myCheckArr[3] == checkArr[3]) checkSecretNumber--;
-                myCheckArr[3]--;
-                break;
-
-        }
-    }
-
-    private static void Add(char c) {
-        switch (c) {
-            case 'A':
-                myCheckArr[0]++;
-                if (myCheckArr[0] == checkArr[0]) checkSecretNumber++;
-                break;
-
-        case 'C':
-                myCheckArr[1]++;
-                if (myCheckArr[1] == checkArr[1]) checkSecretNumber++;
-                break;
-
-            case 'G':
-                myCheckArr[2]++;
-                if (myCheckArr[2] == checkArr[2]) checkSecretNumber++;
-                break;
-
-            case 'T':
-                myCheckArr[3]++;
-                if (myCheckArr[3] == checkArr[3]) checkSecretNumber++;
-                break;
-        }
-    }
 
     private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -89,14 +54,14 @@ public class Q12891 {
         dna = new char[S];
         checkArr = new int[4]; // {A, C, G, T}
         myCheckArr = new int[4];
-
         dna = br.readLine().toCharArray();
+
         st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < 4; i++) {
-            checkArr[i] = Integer.parseInt(st.nextToken());
-            if (checkArr[i] == 0) checkSecretNumber++;
-        }
+        dnaMap.put('A', Integer.parseInt(st.nextToken()));
+        dnaMap.put('C', Integer.parseInt(st.nextToken()));
+        dnaMap.put('G', Integer.parseInt(st.nextToken()));
+        dnaMap.put('T', Integer.parseInt(st.nextToken()));
 
         br.close();
     }
