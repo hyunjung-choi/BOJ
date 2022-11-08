239ea6b459e7b3d4165c93e61e1bb60793263a19
hyunjung-choi <hjchoi.dev@gmail.com>
2022-11-08 11:47:44 +0900

    ✨ Add Q2744 대소문자 바꾸기

---
+++ src/Q2744.kt
@@ -0,0 +1,24 @@
+/**
+ * 대소문자 바꾸기
+ * https://www.acmicpc.net/problem/2744
+ */
+
+private var str = String()
+
+private fun solution(str: String) {
+    str.forEach {
+        when {
+            it.isLowerCase() -> print(it.uppercaseChar())
+            it.isUpperCase() -> print(it.lowercaseChar())
+        }
+    }
+}
+
+private fun input() = with(System.`in`.bufferedReader()) {
+    str = readLine()
+}
+
+fun main() {
+    input()
+    solution(str)
+}
