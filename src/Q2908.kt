08de60fac3aca4cf9d815793fb1079db5b8ce29a
hyunjung-choi <hjchoi.dev@gmail.com>
2022-11-08 10:30:52 +0900

    ✨ Add Q2908 상수

---
+++ src/Q2908.kt
@@ -0,0 +1,33 @@
+import java.lang.Math.abs
+import java.util.*
+
+/**
+ * 상수
+ * https://www.acmicpc.net/problem/2908
+ */
+
+private var firstNumber = 0
+private var secondNumber = 0
+
+fun solution(firstNumber: Int, secondNumber: Int): Int {
+    val reversedFirstNumber = reverseNumber(firstNumber)
+    val reversedSecondNumber = reverseNumber(secondNumber)
+
+    return Math.max(reversedFirstNumber, reversedSecondNumber)
+}
+
+fun reverseNumber(number: Int): Int {
+    val sign = Math.signum(number.toDouble()).toInt()
+    return abs(number).toString().reversed().toInt() * sign
+}
+
+fun input() = with(System.`in`.bufferedReader()) {
+    val st = StringTokenizer(readLine())
+    firstNumber = st.nextToken().toInt()
+    secondNumber = st.nextToken().toInt()
+}
+
+fun main() {
+    input()
+    println(solution(firstNumber, secondNumber))
+}
