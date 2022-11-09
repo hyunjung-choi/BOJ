7c8757a094435a90113a51596aea19bfdc3929bd
hyunjung-choi <hjchoi.dev@gmail.com>
2022-11-09 13:18:20 +0900

    ✨ Add Q1978 소수 찾기

---
+++ src/Q1978.kt
@@ -0,0 +1,40 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
+/**
+ * 소수 찾기
+ * https://www.acmicpc.net/problem/1978
+ */
+
+private var N = 0
+private var answer = 0
+private var numberList = ArrayList<Int>()
+
+private fun solution() {
+    numberList.forEach {
+        if (isPrime(it)) answer++
+    }
+    println(answer)
+}
+
+private fun isPrime(number: Int) : Boolean {
+    if (number <= 1) return false
+    var i = 2
+    while (i * i <= number) {
+        if (number % i++ == 0) return false
+    }
+    return true
+}
+
+private fun input() = with(System.`in`.bufferedReader()) {
+    N = readLine().toInt()
+    val st = StringTokenizer(readLine())
+    for (i in 0 until N) {
+        numberList.add(st.nextToken().toInt())
+    }
+}
+
+fun main() {
+    input()
+    solution()
+}
