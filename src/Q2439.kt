1a16d4daa7317b49c32d343ad85c5cb2738d98ce
hyunjung-choi <hjchoi.dev@gmail.com>
2022-11-16 23:34:25 +0900

    ✨ Add Q2439 별 찍기 - 2

---
+++ src/Q2439.kt
@@ -0,0 +1,28 @@
+/**
+ * 별 찍기 - 2
+ * https://www.acmicpc.net/problem/2439
+ */
+
+private var N: Int = 0
+
+private fun solution() {
+    for (i in 1..N){
+        for (j in 1..N - i) {
+            print(" ")
+        }
+        for (k in 1..i) {
+            print("*")
+        }
+        println()
+    }
+}
+
+private fun input() = with(System.`in`.bufferedReader()) {
+    N = readLine().toInt()
+    close()
+}
+
+fun main() {
+    input()
+    solution()
+}
