7300e1b67bc744e18a477283283108e56853e8fa
hyunjung-choi <hjchoi.dev@gmail.com>
2022-11-18 23:47:43 +0900

    ✨ Add Q9093 단어 뒤집기

---
+++ src/Q9093.kt
@@ -0,0 +1,31 @@
+/**
+ *  단어 뒤집기
+ *  https://www.acmicpc.net/problem/9093
+ */
+
+private var n: Int = 0
+private var strList = mutableListOf<String>()
+
+private fun solution() {
+    for (sentence in strList) {
+        for (str in sentence.split(" ")) {
+            print(str
+                .reversed()
+                .plus(" "))
+        }
+        println()
+    }
+}
+
+private fun input() = with(System.`in`.bufferedReader()) {
+    n = readLine().toInt()
+    repeat(n) {
+        strList.add(readLine())
+    }
+    close()
+}
+
+fun main() {
+    input()
+    solution()
+}
