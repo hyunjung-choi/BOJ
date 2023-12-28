e32c81267e942ea5dca3b66684d45ff908c68dca
최현정 <hjchoi.dev@gmail.com>
2023-12-28 22:20:45 +0900

    ✨ Add Q11650 좌표 정렬하기

---
+++ src/Q11650.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * 좌표 정렬하기
+ * https://www.acmicpc.net/problem/11650
+ */
+
+public class Q11650 {
+    private static int N;
+    private static int[][] coordinate;
+    private static final StringBuilder sb = new StringBuilder();
+
+    private static void solution() {
+        Arrays.sort(coordinate, ((o1, o2) -> {
+            if (o1[0] == o2[0]) return o1[1] - o2[1];
+            else return o1[0] - o2[0];
+        }));
+
+        for (int i = 0; i < N; i++) {
+            sb.append(coordinate[i][0]).append(" ").append(coordinate[i][1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        coordinate = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            coordinate[i][0] = Integer.parseInt(st.nextToken()); // x좌표
+            coordinate[i][1] = Integer.parseInt(st.nextToken()); // y좌표
+        }
+
+        br.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+    }
+}
