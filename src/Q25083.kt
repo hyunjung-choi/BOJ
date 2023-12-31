c1f590c65fa33e5109967646512b44a2772f1d75
최현정 <hjchoi.dev@gmail.com>
2023-12-31 22:22:44 +0900

    ✨ Add Q25083 새싹

---
+++ src/Q25083.kt
@@ -0,0 +1,17 @@
+/**
+ * 새싹
+ * https://www.acmicpc.net/problem/25083
+ */
+
+fun main() {
+    println(
+        """
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+    """.trimIndent()
+    )
+}
