416f693fa9a64995752faea1bf51463fe4c66fbc
최현정 <hjchoi.dev@gmail.com>
2024-07-11 12:50:44 +0900

    ✨ Add Q2751 수 정렬하기 2

---
+++ src/Q2751.kt
@@ -0,0 +1,33 @@
+/**
+ * 수 정렬하기 2
+ * https://www.acmicpc.net/problem/2751
+ *
+ * println 대신 StringBuilder 를 써야 시간 초과가 안난다!
+ */
+
+private var N = 0
+private var list = mutableListOf<Int>()
+private val sb = StringBuilder()
+
+private fun solution() {
+    list = list.sorted().toMutableList()
+    for (number in list) {
+        sb.append(number).append("\n")
+    }
+    println(sb.toString())
+}
+
+private fun input() = with(System.`in`.bufferedReader()) {
+    N = readln().toInt()
+
+    repeat(N) {
+        list.add(readln().toInt())
+    }
+
+    close()
+}
+
+fun main() {
+    input()
+    solution()
+}
